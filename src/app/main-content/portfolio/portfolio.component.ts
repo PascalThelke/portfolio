719f221358c5d829991f8e0b841ec85eceb5c701
Pascal Thelke <p.thelke@googlemail.com>
2024-03-31 21:40:58 +0200

    included portfolio section

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { JoinComponent } from './join/join.component';
+import { SharkieComponent } from './sharkie/sharkie.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [JoinComponent, SharkieComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
---
+++ src/app/main-content/portfolio/sharkie/sharkie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sharkie',
+  standalone: true,
+  imports: [],
+  templateUrl: './sharkie.component.html',
+  styleUrl: './sharkie.component.scss'
+})
+export class SharkieComponent {
+
+}
