c712846150b2b05ef6f8ee93535045aff313d212
Pascal Thelke <p.thelke@googlemail.com>
2024-04-04 17:07:44 +0200

    implemented contact from validation + messages on invalid entrys

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
+  acceptedPolicy = false;
+  mailTest = true;
+  http = inject(HttpClient)
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response: any) => {
+            ngForm.resetForm();
+          },
+          error: (error: any) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
+
+  acceptPolicy(event: any) {
+    const checkboxImage = event.target;
+    if(!this.acceptedPolicy){
+      checkboxImage.src = '../../../assets/img/contact_me/check_box.svg';
+      this.acceptedPolicy = true;
+    }else{
+      checkboxImage.src = '../../../assets/img/contact_me/check_box_outline_blank.svg';
+      this.acceptedPolicy = false;
+    }
+   
+  
+  }
 }
